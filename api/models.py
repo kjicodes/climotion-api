c11307353110376dfa7846152102e3735336bea2
Kateleen Issa <kateleenissa@Jimis-MacBook-Air-2.local>
2026-04-21 07:56:49 -0400

    Add user foreign key to SavedWorkout model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class SearchedCity(models.Model):
     city_name = models.CharField(max_length=100, blank=False)
@@ -12,6 +13,7 @@ class SearchedCity(models.Model):
 
 
 class SavedWorkout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     exercise_type = models.CharField(max_length=100, blank=False)
     difficulty = models.CharField(max_length=100, blank=False)
     workout = models.JSONField(blank=False)
